91f114ac858174997bd2852e7eb054fa765c0103
chan haga <obutora@gmail.com>
2022-02-15 00:36:16 +0900

    add selectable screen

--- lib/provider/progress_provider.dart
+++ lib/provider/progress_provider.dart
@@ -3,7 +3,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 final progressProvider = StateNotifierProvider(((ref) => ProgressNotifier()));
 
 class ProgressNotifier extends StateNotifier<int> {
-  ProgressNotifier() : super(1);
+  ProgressNotifier() : super(0);
   void change(int e) {
     state = e;
   }
